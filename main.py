5565df43c4908d2cb9446e7ab82c803cd5b5c2cd
Ian H <ianlouishawthorne@gmail.com>
2022-12-06 17:49:48 -0600

    bug fix also working on branches

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ class Interpreter:
     def __init__(self) -> None:
         self.vars = {}
         self.functions = {}
+        self.open_brackets = 0
     def getLines(self, code: str) -> list:
         ret = []
         txt = ""
@@ -176,94 +177,103 @@ class Interpreter:
         if txt != "":
             tokens.append(txt)
         return tokens
-    def runLine(self, line: str|list) -> None:
+    def runLine(self, line: str|list):
         if line is list:
-            tokens = line
+                tokens = line
         else: tokens = self.tokenize(line)
         if (len(tokens) == 0):
             return
-        # replaces all variables with their values
-        j = 0
-        while j < len(tokens):
-            i = tokens[j]
-            if i in self.vars and not (j == 0 and "=" in tokens):
-                tokens[j] = self.vars[i]
-            j += 1
-        if tokens[0] == "(":
-            open_paren = 1
-            for i in tokens:
-                if i == "(":
-                    open_paren += 1
-                elif i == ")":
-                    open_paren -= 1
-                if open_paren == 0:
-                    break
-            else:
-                print("ERROR: No closing parenthesis")
-                return "ERROR: No closing parenthesis"
-        elif tokens[0] == "print":
-            if tokens[1] == "(":
-                if tokens[2] == ")":
-                    print()
+        if self.open_brackets < 1:
+            # replaces all variables with their values
+            j = 0
+            while j < len(tokens):
+                i = tokens[j]
+                if i in self.vars and not (j == 0 and "=" in tokens):
+                    tokens[j] = self.vars[i]
+                j += 1
+            if tokens[0] == "(":
+                open_paren = 1
+                for i in tokens:
+                    if i == "(":
+                        open_paren += 1
+                    elif i == ")":
+                        open_paren -= 1
+                    if open_paren == 0:
+                        break
                 else:
-                    innerTokens = tokens[2:-1]
-                    print(self.runLine(innerTokens))
-            else:
-                print("BUILTIN FUNCTION: print")
-            return
-        elif tokens[0] == "input":
-            if tokens[1] == "(":
-                if tokens[2] == ")":
-                    return input()
+                    print("ERROR: No closing parenthesis")
+                    return "ERROR: No closing parenthesis"
+            elif tokens[0] == "print":
+                if len(tokens) > 1 and tokens[1] == "(":
+                    if tokens[2] == ")":
+                        print()
+                    else:
+                        innerTokens = tokens[2:-1]
+                        print(self.runLine(innerTokens))
                 else:
-                    innerTokens = tokens[2:-1]
-                    return input(self.runLine(innerTokens))
-            else:
-                print("BUILTIN FUNCTION: input")
-            return
-        elif tokens[0] == None:
-            print("ERROR: unexpected none-type")
-            return "ERROR: unexpected none-type"
-        elif tokens[0].isnumeric():
-            # create ast
-            return self.runAST(tokens)
-        elif self.isstring(tokens[0]):
-            return self.string(tokens)
-        elif tokens[0] == "let":
-            print(tokens)
-            if tokens[1] in self.vars:
-                print("ERROR: variable '" + tokens[1] + "' already exists")
-                return "ERROR: variable '" + tokens[1] + "' already exists"
-            if len(tokens) > 2 and tokens[2] == "=":
-                self.vars[tokens[1]] = self.runLine(tokens[3:])
-            else:
-                self.vars[tokens[1]] = None
-        elif tokens[0] in self.vars:
-            if tokens[1] in ["+","-", "*", "/", "%", "^","="]:
-                if tokens[1] == "=":
-                    self.vars[tokens[0]] = self.runLine(tokens[2:])
-                elif tokens[2] == "=":
-                    if tokens[1] == "+":
-                        self.vars[tokens[0]] = self.vars[tokens[0]] + self.runLine(tokens[3:])
-                    elif tokens[1] == "-":
-                        self.vars[tokens[0]] = self.vars[tokens[0]] - self.runLine(tokens[3:])
-                    elif tokens[1] == "*":
-                        self.vars[tokens[0]] = self.vars[tokens[0]] * self.runLine(tokens[3:])
-                    elif tokens[1] == "/":
-                        self.vars[tokens[0]] = self.vars[tokens[0]] / self.runLine(tokens[3:])
-                    elif tokens[1] == "%":
-                        self.vars[tokens[0]] = self.vars[tokens[0]] % self.runLine(tokens[3:])
-                    elif tokens[1] == "^":
-                        self.vars[tokens[0]] = self.vars[tokens[0]] ** self.runLine(tokens[3:])
+                    return("BUILTIN FUNCTION: print")
+                return
+            elif tokens[0] == "input":
+                if tokens[1] == "(":
+                    if tokens[2] == ")":
+                        return input()
+                    else:
+                        innerTokens = tokens[2:-1]
+                        return input(self.runLine(innerTokens))
                 else:
-                    self.vars[tokens[0]] = self.runLine(self.vars[tokens[0]] + tokens[1] + tokens[2])
-        else:
-            print("ERROR: unknown identifier '" + tokens[0] + "'")
-            return "ERROR: unknown identifier '" + tokens[0] + "'"
+                    return("BUILTIN FUNCTION: input")
+                return
+            elif tokens[0] == None:
+                print("ERROR: unexpected none-type")
+                return "ERROR: unexpected none-type"
+            elif tokens[0].isnumeric():
+                # create ast
+                return self.runAST(tokens)
+            elif self.isstring(tokens[0]):
+                return self.string(tokens)
+            elif tokens[0] == "let":
+                if tokens[1] in self.vars:
+                    print("ERROR: variable '" + tokens[1] + "' already exists")
+                    return "ERROR: variable '" + tokens[1] + "' already exists"
+                if len(tokens) > 2 and tokens[2] == "=":
+                    self.vars[tokens[1]] = self.runLine(tokens[3:])
+                else:
+                    self.vars[tokens[1]] = None
+            elif tokens[0] in self.vars:
+                if tokens[1] in ["+","-", "*", "/", "%", "^","="]:
+                    if tokens[1] == "=":
+                        self.vars[tokens[0]] = self.runLine(tokens[2:])
+                    elif tokens[2] == "=":
+                        if tokens[1] == "+":
+                            self.vars[tokens[0]] = self.vars[tokens[0]] + self.runLine(tokens[3:])
+                        elif tokens[1] == "-":
+                            self.vars[tokens[0]] = self.vars[tokens[0]] - self.runLine(tokens[3:])
+                        elif tokens[1] == "*":
+                            self.vars[tokens[0]] = self.vars[tokens[0]] * self.runLine(tokens[3:])
+                        elif tokens[1] == "/":
+                            self.vars[tokens[0]] = self.vars[tokens[0]] / self.runLine(tokens[3:])
+                        elif tokens[1] == "%":
+                            self.vars[tokens[0]] = self.vars[tokens[0]] % self.runLine(tokens[3:])
+                        elif tokens[1] == "^":
+                            self.vars[tokens[0]] = self.vars[tokens[0]] ** self.runLine(tokens[3:])
+                    else:
+                        self.vars[tokens[0]] = self.runLine(self.vars[tokens[0]] + tokens[1] + tokens[2])
+            elif "{" in tokens:
+                pass
+            elif "}" in tokens:
+                pass
+            else:
+                print("ERROR: unknown identifier '" + tokens[0] + "'")
+                return "ERROR: unknown identifier '" + tokens[0] + "'"
+        if "{" in tokens:
+            self.open_brackets += 1
+        if "}" in tokens:
+            self.open_brackets -= 1
+            
             
     def runAST(self, tokens: list) -> None:
         for i in tokens:
-            if not i.isnumeric() and i not in ["+", "-", "*", "/", "%", "^", "&", "|", "!", "<", ">", "?", ".", "==", "="]:
+            if not i.isnumeric() and i not in ["+", "-", "*", "/", "%", "^", "&", "|", "!", "<", ">", "?", ".", "==", "=","(",")"]:
                 print(f"ERROR: expected number, got '{i}'")
                 return f"ERROR: expected number, got '{i}'"
 
@@ -279,7 +289,7 @@ class Interpreter:
         j = 0
         if self.isstring(tokens[0]):
             final += tokens[0][1:-1]
-            j += 1
+            j = 1
         while j < len(tokens):
             if tokens[j] == "+":
                 j += 1
